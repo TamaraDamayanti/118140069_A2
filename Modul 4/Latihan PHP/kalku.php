845cff2ab009938446ecf17a25270e0f390e8c7c
araa <64761777+TamaraDamayanti@users.noreply.github.com>
2020-11-14 20:23:11 +0700

    Add files via upload

--- Modul 4/Latihan PHP/kalku.php
+++ Modul 4/Latihan PHP/kalku.php
@@ -1,1 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <title></title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+  <article>
+   <?php
+    if (isset($_POST['hitung'])) {
+      $angka1 = $_POST['angka1'];
+      $angka2 = $_POST['angka2'];
+      $operasi = $_POST['operasi'];
+      switch ($operasi) {
+        case '+':
+          $hasil = $angka1 + $angka2;
+          break;
+        case '-':
+          $hasil = $angka1 - $angka2;
+          break;
+        case '*':
+          $hasil = $angka1 * $angka2;
+          break;
+        case '/':
+          $hasil = $angka1 / $angka2;
+          break;
+
+        case '%':
+          $hasil = $angka1 % $angka2;
+          break;
+
+        default:
+          $hasil = "Pilih Operator";
+          break;
+      }
+    }
+    ?>
+    <div class="kalkulator">
+      <h1 class="judul">KALKULATOR</h1>
+
+      <form method="post" action="kalku.php">
+        <input type="text" name="angka1" class="angka" autocomplete="off" placeholder="Masukkan Angka">
+        <input type="text" name="angka2" class="angka" autocomplete="off" placeholder="Masukkan Angka Lagi">
+        <select class="opt" name="operasi">
+          <option value="#">Operator</option>
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">x</option>
+          <option value="/">/</option>
+          <option value="%">Mod(%)</option>
+        </select>
+        <input type="submit" name="hitung" value="Hitung" class="tombol">
+      </form>
+       
+      <?php if (isset($_POST['hitung'])) { ?>
+        <input type="text" value="<?php echo $hasil; ?>" class="angka">
+      <?php } else { ?>
+        <input type="text" value="0" class="angka">
+      <?php } ?>
+    </div>
+  </article>
+  </div>
+</body>
+
+</html>
