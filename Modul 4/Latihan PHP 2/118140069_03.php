3edef2a4409bcd72a752fa0aa00f0c762bfacaac
araa <64761777+TamaraDamayanti@users.noreply.github.com>
2020-11-14 20:29:23 +0700

    Add files via upload

--- Modul 4/Latihan PHP 2/118140069_03.php
+++ Modul 4/Latihan PHP 2/118140069_03.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h3>Menentukan Bilangan Prima</h3>
+	<?php
+		if (isset($_POST['submit'])) {
+			$angka = $_POST['angka'];
 
+  		$prima = true;
+  		for ($i = 2; $i < $angka; $i++) {
+    		if ($angka % $i == 0)
+      			$prima = false;
+  		}	
+
+  		if ($prima) {
+    		$hasil = " Bilangan Prima";
+  		} else {
+    		$hasil = "Bukan Bilangan Prima";
+  			}
+		}
+	?>
+
+	<form method='post' action='118140069_03.php'>
+		 	Bilangan : <input type='text' name='angka' class='angka' autocomplete='off'><br><br>
+		 	<input type='submit' name='submit' value='submit' class='tombol'>
+		 <br><br>
+	</form>
+
+	<?php if (isset($_POST['submit'])) { ?>
+		<input type="text" value="<?php echo $hasil; ?>" class="angka">
+	<?php } else { ?>
+		Output : <input type="text" value=" " class="angka">
+	<?php } ?>
+
+</body>
+</html>
